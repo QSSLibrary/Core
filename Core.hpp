0564f9b8b61235f2af1a371253c0966da1f56ed2
RamtinKosari <ramtinkosari@gmail.com>
2024-01-06 17:19:56 +0330

    [#REFACTOR] Add License Header File

--- Core.hpp
+++ Core.hpp
@@ -1,4 +1,16 @@
 # ifndef QSSL_CORE
+    //-- Check QSSL License
+    # ifndef QSSL_LICENSE
+        # include "License.hpp"
+        # if (QSSL_LICENSE_AGREEMENT == false)
+            # define COMPILE_TIME NULL
+            # define COMPILE_DATE NULL
+            # error First You Must Read License in Path "Core/License.hpp" and Make Related Flag to True
+        # else 
+            # define INCLUDE_TIME __TIME__
+            # define INCLUDE_DATE __DATE__
+        # endif // QSSL_LICENSE_AGREEMENT
+    # endif // QSSL_LICENSE
     /**
      * @file Core.hpp
      * @author Ramtin Kosari (ramtinkosari@gmail.com)
