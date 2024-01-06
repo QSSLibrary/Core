1ba43a51dfcf9fd5776ad1d44927f5d47a39302f
RamtinKosari <ramtinkosari@gmail.com>
2024-01-06 17:43:26 +0330

    [#REFACTOR] Update Doxygen of Core

--- Core.hpp
+++ Core.hpp
@@ -15,7 +15,6 @@
      * @file Core.hpp
      * @author Ramtin Kosari (ramtinkosari@gmail.com)
      * @brief QSS Open Source Astronomy and Astrophysics Library's Core
-     * @note Enjoy Using this Library
      * @date 2024-01-06
      */
     # define QSSL_CORE
