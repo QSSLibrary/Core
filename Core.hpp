f45c7f3fea64fb65b2f7824cd38cb95d37836969
RamtinKosari <ramtinkosari@gmail.com>
2024-01-06 17:12:35 +0330

    [#REFACTOR] Add Core Header

---
+++ Core.hpp
@@ -0,0 +1,10 @@
+# ifndef QSSL_CORE
+    /**
+     * @file Core.hpp
+     * @author Ramtin Kosari (ramtinkosari@gmail.com)
+     * @brief QSS Open Source Astronomy and Astrophysics Library's Core
+     * @note Enjoy Using this Library
+     * @date 2024-01-06
+     */
+    # define QSSL_CORE
+# endif // QSSL_CORE
